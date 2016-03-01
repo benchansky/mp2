790975e8aa7c5d331edb21da3b45e7615d9fc42c
System Administrator <benchansky@gmail.com>
2016-03-01 01:29:11 -0600

    getting there, still need to do smooth scrolling and nav bar

--- source_js/script.js
+++ source_js/script.js
@@ -1,1 +1,20 @@
 // Write any custom javascript functions here
+$(document).ready(function(){
+
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
